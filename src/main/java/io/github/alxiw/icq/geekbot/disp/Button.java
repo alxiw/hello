abaa3c8c7e7cb8d45e64da2c73f0a4998b8a02b5
Alexander Ivanovsky <a.ivanovsky@corp.mail.ru>
2020-11-22 17:46:01 +0300

    Fix vulnerability junit dependency and rename joke buttons

--- src/main/java/io/github/alxiw/icq/geekbot/disp/Button.java
+++ src/main/java/io/github/alxiw/icq/geekbot/disp/Button.java
@@ -4,8 +4,8 @@ public enum Button {
     HELLO_START("\uD83D\uDE80 Start", "callback_hello_start"),
     ERROR_RETRY("\uD83C\uDFB2 Retry", "callback_error_retry"),
     ERROR_OK("✋ OK", "callback_error_ok"),
-    SUCCESS_REFRESH("\uD83D\uDCA9 It's lame, refresh", "callback_success_refresh"),
-    SUCCESS_NEXT("\uD83D\uDE02 Keep and send next", "callback_success_next");
+    SUCCESS_REFRESH("\uD83D\uDCA9 Bad", "callback_success_refresh"),
+    SUCCESS_NEXT("\uD83D\uDE02 Good", "callback_success_next");
 
     private final String description;
     private final String code;
