c0f9dabdab6db10b1958a3c20a573d8cd8fe900d
Alexander Ivanovsky <alxiw@outlook.com>
2024-11-16 02:29:05 +0300

    fix db url
    (cherry picked from commit d990654321ef5d4c08a4c37d97bbcdfd2c51790c)

--- src/main/java/io/github/alxiw/hello/data/Constants.java
+++ src/main/java/io/github/alxiw/hello/data/Constants.java
@@ -1,6 +1,6 @@
 package io.github.alxiw.hello.data;
 
 class Constants {
-    static final String DB_URL = "jdbc:sqlite:data/hello.db";
+    static final String DB_URL = "jdbc:sqlite:/data/hello.db";
     static final String JSON_URL = "/data.json";
 }
